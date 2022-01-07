265edc537fac91333e223e7aa8afe3138361324e
Aaron Minnick <abminnick@gmail.com>
2022-01-07 09:34:38 -0800

    add HomeController, Home Index View

---
+++ Factory/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      ViewBag.Engineers = _db.Engineers.ToList();
+      ViewBag.Machines = _db.Machines.ToList();
+      return View();
+    }
+  }
+}

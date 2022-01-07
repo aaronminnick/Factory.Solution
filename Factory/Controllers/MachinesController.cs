fe154cff73afde6ce0e9c4fbd192efdd89a9b6e4
Aaron Minnick <abminnick@gmail.com>
2022-01-07 14:59:57 -0800

    add Details, Edit, License management from Machine side

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -26,5 +26,47 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index", "Home");
     }
+
+    public ActionResult Details(int id)
+    {
+      Machine model = _db.Machines
+        .Include(machine => machine.Licenses)
+        .FirstOrDefault(machine => machine.MachineId == id);
+      ViewData["Engineers"] = _db.Engineers.ToList();
+      return View(model);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Machine machine)
+    {
+      _db.Entry(machine).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Details", new {id = machine.MachineId});
+    }
+
+    [HttpPost("Machines/AddLicense")]
+    public PartialViewResult AddLicense(int EngineerId, int MachineId)
+    {
+      _db.Licenses.Add(new EngineerMachine() {EngineerId = EngineerId, MachineId = MachineId});
+      _db.SaveChanges();
+      Machine model = _db.Machines
+        .Include(machine => machine.Licenses)
+        .FirstOrDefault(machine => machine.MachineId == MachineId);
+      ViewData["Engineers"] = _db.Engineers.ToList();
+      return PartialView("_ManageLicensesPartial", model);
+    }
+
+    [HttpPost("Machines/RemoveLicense")]
+    public PartialViewResult RemoveLicense(int EngineerId, int MachineId)
+    {
+      EngineerMachine license = _db.Licenses.FirstOrDefault(license => license.EngineerId == EngineerId && license.MachineId == MachineId);
+      _db.Licenses.Remove(license);
+      _db.SaveChanges();
+      Machine model = _db.Machines
+        .Include(machine => machine.Licenses)
+        .FirstOrDefault(machine => machine.MachineId == MachineId);
+      ViewData["Engineers"] = _db.Engineers.ToList();
+      return PartialView("_ManageLicensesPartial", model);
+    }
   }
 }
